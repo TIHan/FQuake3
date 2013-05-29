8d4ff5eb55207d2c384fbb673454f7078a4613b8
TIHan <lol.tihan@gmail.com>
2013-05-29 00:07:45 -0500

    Fixed a few paths and libraries.

--- src/C/FQuake3/main.c
+++ src/C/FQuake3/main.c
@@ -13,7 +13,7 @@ main (int argc, char *argv[])
 	m_instance = m_instance_new ();
 
 	// Initialize Mono
-	m_init (m_instance, "Mono-3.0.9\\lib", "Mono-3.0.9\\etc", "FQuake3", M_RUNTIME_4_0);
+	m_init (m_instance, "Mono\\lib", "Mono\\etc", "FQuake3", M_RUNTIME_4_0);
 
 	// Load System Assembly
 	m_load_assembly (m_instance, "idTech3.System.dll");
