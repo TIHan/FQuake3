f6df55801117289ba40b99598ddd51cbb2649c88
TIHan <lol.tihan@gmail.com>
2013-08-25 06:34:42 -0500

    Added qm_cgame.c/h.

---
+++ src/C/Native/qm_cgame.c
@@ -0,0 +1,33 @@
+/*
+===========================================================================
+Copyright (C) 2013 William F. Smith
+Copyright (C) 1999-2005 Id Software, Inc.
+
+This file is part of Quake III Arena source code.
+
+Quake III Arena source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Quake III Arena source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+
+// IMPORTANT: This file is only temporary and will not exist once most of the existing codebase
+//	gets converted to F#. It is only intended as interop helpers.
+
+#include "qm_cgame.h"
+
+MObject
+qm_map_cg (cg_t* cg)
+{
+	return m_invoke_method ("CGame", "CGame.Native", "CGame", "ofNative", &cg);
+}
