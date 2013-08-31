658e1a950b33c97f2e3b7362c5014ae386984783
TIHan <lol.tihan@gmail.com>
2013-08-31 16:14:06 -0500

    Added Launch.fsx.

---
+++ src/FSharp/Launcher/Launch.fsx
@@ -0,0 +1,13 @@
+﻿module Launcher.Runner
+
+#r "Engine.dll"
+#r "Engine.Renderer.dll"
+#r "CGame.dll" // temporary
+#r "FSharpx.Core.dll"
+
+open Engine.System
+
+let start () =
+    async {
+        System.Start ()
+    } |> Async.Start;
