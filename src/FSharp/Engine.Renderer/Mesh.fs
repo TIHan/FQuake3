47d507c9fa33b6beea4672179289b35a1cd871f1
TIHan <lol.tihan@gmail.com>
2013-10-25 21:09:05 -0500

    R_CullModel almost in F#; need to add perf counters.

--- src/FSharp/Engine.Renderer/Mesh.fs
+++ src/FSharp/Engine.Renderer/Mesh.fs
@@ -32,7 +32,82 @@ open Engine.Math
 open Engine.NativeInterop
 
 module Mesh =
+    /// CalculateCullLocalBox
+    let calculateCullLocalBox (newFrame: Md3Frame) (oldFrame: Md3Frame) (noCull: Cvar) (tr: TrGlobals) =
+        let inline calculateBounds i j =
+            match oldFrame.Bounds.[i].[j] < newFrame.Bounds.[i].[j] with
+            | true -> oldFrame.Bounds.[i].[j]
+            | _ -> newFrame.Bounds.[i].[j]
+
+        // calculate a bounding box in the current coordinate system
+        let bounds =
+            {
+                Bounds1 =
+                    {
+                        X = calculateBounds 0 0;
+                        Y = calculateBounds 0 1;
+                        Z = calculateBounds 0 2;
+                    };
+                Bounds2 =
+                    {
+                        X = calculateBounds 1 0;
+                        Y = calculateBounds 1 1;
+                        Z = calculateBounds 1 2;
+                    }
+            }
+
+        match Main.cullLocalBox bounds tr.Orientation tr.ViewParms.Frustum noCull with
+        | ClipType.In ->
+            // TODO: add perf counter
+            ClipType.In
+        | ClipType.Clip ->
+            // TODO: add perf counter
+            ClipType.Clip
+        | _ ->
+            // TODO: add perf counter
+            ClipType.Out
+
     /// Based on Q3: R_CullModel
     /// CullModel
-    let cullModel (newFrame: Md3Frame) (oldFrame: Md3Frame) (entity: RefEntity) (perfCounters: FrontEndPerformanceCounters) =
-        ()
+    /// Note: This is internal.
+    let cullModelByFrames (newFrame: Md3Frame) (oldFrame: Md3Frame) (entity: RefEntity) (noCull: Cvar) (tr: TrGlobals) =
+        // cull bounding sphere ONLY if this is not an upscaled entity
+        match not entity.HasNonNormalizedAxes with
+        | true ->
+            let sphereCull = Main.cullLocalPointAndRadius newFrame.LocalOrigin newFrame.Radius tr.Orientation tr.ViewParms.Frustum noCull
+            match entity.Frame = entity.OldFrame with
+            | true ->
+                match sphereCull with
+                | ClipType.Out ->
+                    // TODO: add perf counter
+                    ClipType.Out
+                | ClipType.In ->
+                    // TODO: add perf counter
+                    ClipType.In
+                | ClipType.Clip ->
+                    // TODO: add perf counter
+                    calculateCullLocalBox newFrame oldFrame noCull tr
+                | _ -> raise <| Exception "Bad clip type."
+            | _ ->
+                let sphereCullB =
+                    match newFrame = oldFrame with
+                    | true -> sphereCull
+                    | _ -> Main.cullLocalPointAndRadius oldFrame.LocalOrigin oldFrame.Radius tr.Orientation tr.ViewParms.Frustum noCull
+                
+                match sphereCull = sphereCullB with
+                | true ->
+                    match sphereCull with
+                    | ClipType.Out ->
+                        // TODO: add perf counter
+                        ClipType.Out
+                    | ClipType.In ->
+                        // TODO: add perf counter
+                        ClipType.In
+                    | ClipType.Clip ->
+                        // TODO: add perf counter
+                        calculateCullLocalBox newFrame oldFrame noCull tr
+                    | _ -> raise <| Exception "Bad clip type."
+                | _ ->
+                    calculateCullLocalBox newFrame oldFrame noCull tr
+        | _ ->
+            calculateCullLocalBox newFrame oldFrame noCull tr
