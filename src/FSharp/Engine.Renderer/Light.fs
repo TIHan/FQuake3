bd1c91ae3f8133b21cf4a97524b7e007a1a4df47
TIHan <lol.tihan@gmail.com>
2013-12-08 20:04:45 -0600

    Forgot to add Light.fs.

---
+++ src/FSharp/Engine.Renderer/Light.fs
@@ -0,0 +1,57 @@
+﻿(*
+Copyright (C) 2013 William F. Smith
+
+This program is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+Derivative of Quake III Arena source:
+Copyright (C) 1999-2005 Id Software, Inc.
+*)
+
+module Engine.Renderer.Light
+
+open System
+open System.Diagnostics.Contracts
+open Engine.Core
+open Engine.Files
+open Engine.Math
+open Engine.Renderer.Core
+
+/// Based on Q3: R_SetupEntityLighting
+/// SetupEntityLighting
+///
+/// Calculates all the lighting values that will be used
+/// by the Calc_* functions
+let setupEntityLighting (rentity: TrRefEntity) =
+    // lighting calculations
+    match rentity.IsLightingCalculated with
+    | true -> rentity
+    | _ ->
+
+    let rentity = { rentity with IsLightingCalculated = true }
+    let entity = rentity.Entity
+
+    //
+    // trace a sample point down to find ambient light
+    //
+    let lightOrigin =
+        match entity.RenderFx.HasFlag RenderFxFlags.LightingOrigin with
+        // seperate lightOrigins are needed so an object that is
+        // sinking into the ground can still be lit, and so
+        // multi-part models can be lit identically
+        | true -> entity.LightingOrigin
+        | _ -> entity.Origin
+
+    // TODO:
+    rentity
